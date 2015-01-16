a8fa2586035d70485e247a2406fbdc193890394b
Sasha Fahrenkopf <sfahrenk@gmail.com>
2015-01-16 11:39:33 -0800

    Added error on user load

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,10 +11,16 @@ angular.module('gitStuffApp')
   .controller('MainCtrl', function ($scope, gitService) {
 
     $scope.getGitInfo = function() {
+      $scope.loaded = false;
       $scope.user = gitService.getUser($scope.username).then(function(data){
         $scope.user = data;
+        console.log(data);
+            $scope.loaded = true;
+      },
+      function(error) {
+        console.log(error);
+        $scope.error = error;
       });
-      $scope.loaded = true;
     }
   });
 
